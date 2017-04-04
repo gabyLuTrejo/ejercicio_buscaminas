021e54a1fbb101bd38e01fbec5a428988adbc588
Gaby Trejo <gabytrejo303@gmail.com>
2017-04-04 12:40:01 -0500

    Bomba activada: alerta + bloqueo

--- assets/js/buscaminas.js
+++ assets/js/buscaminas.js
@@ -13,18 +13,30 @@ function tipoElemento(){
   var elemento = this.getAttribute("elemento");
   switch (elemento) {
     case "bomba":
-
+      var imagen= document.createElement("img");
+      imagen.setAttribute("src", "assets/img/bomba.png");
+      imagen.setAttribute("height","45px");
+      imagen.setAttribute("width", "41px");
+      this.appendChild(imagen);
+      bloqueoDePantalla();
       break;
     case "numero":
       this.textContent = 1;
-      this.style.fontSize = "16px";
+      this.style.fontSize = "18px";
+      this.style.fontWeight = "bold";
       this.style.textAlign = "center";
       break;
     case "vacio":
       this.style.backgroundColor = "#E0ECF8";
       break;
     default:
-
   }
+}
 
+// Se quitan las funciones "click" (bloqueoDePantalla)
+function bloqueoDePantalla(){
+  for (var i = 0, tds=cuadro.length; i < tds; i++) {
+    cuadro[i].removeEventListener("click",tipoElemento);
+  }
+  alert("¡ESTO HA EXPLOATADO!");
 }
