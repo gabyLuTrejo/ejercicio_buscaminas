50eeee47bd7e660f2f938acfc192fe7384653187
Gaby Trejo <gabytrejo303@gmail.com>
2017-04-04 12:10:19 -0500

    Click funcional para números y cambio de color

--- assets/js/buscaminas.js
+++ assets/js/buscaminas.js
@@ -3,13 +3,28 @@ console.log("Hola");
 var cuadro = document.getElementsByTagName('td');
 
 // Agregando función "click" a cada cuadro de la tabla
-for (var i = 0; i < cuadro.length; i++) {
+for (var i = 0, tds=cuadro.length; i < tds; i++) {
   cuadro[i].addEventListener("click",tipoElemento);
 }
 
-//Obteniendo el tipo de elemento que va en el cuadro
+// Obteniendo el tipo de elemento que va en el cuadro
+// Aplicandole el efecto requerido
 function tipoElemento(){
   var elemento = this.getAttribute("elemento");
-  console.log(elemento);
-  
+  switch (elemento) {
+    case "bomba":
+
+      break;
+    case "numero":
+      this.textContent = 1;
+      this.style.fontSize = "16px";
+      this.style.textAlign = "center";
+      break;
+    case "vacio":
+      this.style.backgroundColor = "#E0ECF8";
+      break;
+    default:
+
+  }
+
 }
