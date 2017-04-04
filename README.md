f73795da4209cfc51ce3f381d6e3b33644f0743a
Gaby Trejo <gabytrejo303@gmail.com>
2017-04-04 15:21:44 -0500

    Mensaje en click fuera de la tabla

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Ejercicio Buscaminas
 
 El siguiente programa simula el juego de buscaminas.
-<h4> En este primer alcance: </h4>
+<h4> Primer alcance: </h4>
 <li> el tablero es fijo (4x4) </li>
 <li> las bombas estan predefinidas (1 bomba) </li>
 <li> los números están predefinidos y se muestran al hacerles click </li>
 <li> las celdas vacias cambian de color al hacerles click </li>
 <li> al hacer click sobre la bomba se muestra una alerta, y se bloquea la funcionalidad del juego </li>
 <li> el boton reiniciar, reinicia el juego en cualquier momento </li>
+<h4> Segundo alcance: </h4>
+<li> al hacer click fuera de la tabla se muestra mensaje </li>
--- assets/js/buscaminas.js
+++ assets/js/buscaminas.js
@@ -1,18 +1,23 @@
-console.log("Hola");
 
 var cuadro = document.getElementsByTagName('td');
 var botonReinicio = document.getElementById("reiniciarJuego");
+var clickZona = document.body;
 
-// Agregando función "click" a cada cuadro de la tabla
+// Click para fuera de tabla
+clickZona.addEventListener("click", clickAfueraDeTabla);
+
+// Iniciando cada cuadro de la tabla (formato Default)
 iniciarJuego();
 
 // Agregando función "click" al boton de reinicio
 botonReinicio.addEventListener("click", iniciarJuego);
+botonReinicio.addEventListener("click", noContarClicks);
 
 // Obteniendo el tipo de elemento que va en el cuadro
 // Aplicandole el efecto requerido
 function tipoElemento(){
   var elemento = this.getAttribute("elemento");
+  noContarClicks();
   switch (elemento) {
     case "bomba":
       var imagen= document.createElement("img");
@@ -33,9 +38,11 @@ function tipoElemento(){
 }
 
 // Se quitan las funciones "click" (bloqueoDePantalla)
+// Se evita que active la función "clickAfueraDeTabla"
 function bloqueoDePantalla(){
   for (var i = 0, tds=cuadro.length; i < tds; i++) {
     cuadro[i].removeEventListener("click",tipoElemento);
+    cuadro[i].addEventListener("click", noContarClicks);
   }
   alert("¡ESTO HA EXPLOATADO!");
 }
@@ -47,4 +54,13 @@ function iniciarJuego(){
     cuadro[i].textContent = "";
     cuadro[i].style.backgroundColor = "white";
   }
+}
+
+// Función que manda mensaje al usuario al hacer click fuera de la tabla
+function clickAfueraDeTabla(){
+  alert("Sigue jugando");
+}
+
+function noContarClicks(){
+    event.stopPropagation();
 }
